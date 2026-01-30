c24866607dda6bf035b6a8d8bf562403a05fd3d6
chemanyu-os <chemanyu-os@360os.com>
2026-01-30 11:11:10 +0800

    日志时间

--- service/api/internal/handler/config/service_fee_handler.go
+++ service/api/internal/handler/config/service_fee_handler.go
@@ -3,26 +3,37 @@ package config
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"media_report/service/api/internal/logic/config"
 	"media_report/service/api/internal/svc"
 	"media_report/service/api/internal/types"
 
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 // 获取服务费配置列表
 func GetServiceFeeListHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		clientIP := r.RemoteAddr
+		if ip := r.Header.Get("X-Forwarded-For"); ip != "" {
+			clientIP = ip
+		}
+		logx.Infof("[ServiceFeeList] 请求开始: client=%s, url=%s, ua=%s", clientIP, r.URL.String(), r.UserAgent())
 		defer func() {
 			if err := recover(); err != nil {
+				logx.Errorf("[ServiceFeeList] panic: %v", err)
 				httpx.ErrorCtx(r.Context(), w, fmt.Errorf("服务器内部错误: %v", err))
 			}
+			logx.Infof("[ServiceFeeList] 请求结束: client=%s, url=%s, 耗时=%v", clientIP, r.URL.String(), time.Since(start))
 		}()
 
 		l := config.NewGetServiceFeeListLogic(r.Context(), svcCtx)
 		resp, err := l.GetServiceFeeList()
 		if err != nil {
+			logx.Errorf("[ServiceFeeList] 业务错误: %v", err)
 			httpx.ErrorCtx(r.Context(), w, err)
 		} else {
 			httpx.OkJsonCtx(r.Context(), w, resp)
