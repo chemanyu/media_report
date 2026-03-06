54077c5bb9a082f7c329b57c9a4314bfa58b3e71
chemanyu-os <chemanyu-os@360os.com>
2026-03-06 20:04:40 +0800

    fix

--- service/api/internal/handler/routes.go
+++ service/api/internal/handler/routes.go
@@ -356,7 +356,7 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 			{
 				// 下载报表文件
 				Method:  http.MethodGet,
-				Path:    "zfb/download/:filename",
+				Path:    "/zfb/download/:filename",
 				Handler: download.DownloadReportHandler(serverCtx),
 			},
 		},
