a0c11f0a54fed0f99303a08824b6dd060a309fb6
chemanyu-os <chemanyu-os@360os.com>
2026-03-27 13:18:28 +0800

    fix

--- service/api/internal/handler/update/update_dhh_cookie_handler.go
+++ service/api/internal/handler/update/update_dhh_cookie_handler.go
@@ -7,6 +7,7 @@ import (
 	"media_report/service/api/internal/svc"
 	"media_report/service/api/internal/types"
 
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
@@ -19,6 +20,7 @@ func UpdateDhhCookieHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 		}
 
 		l := logic.NewUpdateDhhCookieLogic(r.Context(), svcCtx)
+		logx.Infof("请求参数: %s, %s", req.CsrfToken, req.Token)
 		resp, err := l.UpdateDhhCookie(&req)
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
